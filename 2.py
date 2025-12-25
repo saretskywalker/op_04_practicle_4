c37a3c0e0c258f8726cecaad08fde2136837b6f4
saretskywalker <138796350+saretskywalker@users.noreply.github.com>
2025-12-26 01:02:15 +0300

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,7 @@
+from math import sqrt
+
+x1 = int(input("Введите х1"))
+y1 = int(input("Введите y1"))
+x2 = int(input("Введите x2"))
+y2 = int(input("Введите y2"))
+print("Евклидово расстояние равно", sqrt((x1-x2)**2 + (y1-y2)**2))
