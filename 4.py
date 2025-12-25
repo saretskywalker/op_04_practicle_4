0ef69a6f6fda9b4a0353e5dd89f7876f2e186987
saretskywalker <138796350+saretskywalker@users.noreply.github.com>
2025-12-26 01:24:24 +0300

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,3 @@
+students = int(input("Введите колво школьников "))
+orange = int(input("Введите колво мандаринов "))
+print(f"каждому школьнику достанется {orange // students} в корзине останется {students % orange}")
